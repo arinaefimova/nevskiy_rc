261c87f00a3d54c4d54847fd677ca6ddc5246a9e
arina <arina.efimova162002@mail.ru>
2024-11-14 12:51:44 +0300

    adaptiv

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ import { initMap } from "./map";
 import { initializeSwiper } from "./slider";
 import { validateForm } from "./validation";
 
+const iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
 document.addEventListener("DOMContentLoaded", () => {
     initializeSwiper();
     initMap();
--- js/slider.js
+++ js/slider.js
@@ -6,15 +6,27 @@ import 'swiper/css/navigation';
 export function initializeSwiper() {
     new Swiper('.swiper', {
         modules: [Navigation],
-        slidesPerView: 3,
-        spaceBetween: 90,
         observer: true,
         observeParents: true,
-        centeredSlides: true,
         loop: true,
         navigation: {
             nextEl: ".next",
             prevEl: ".prev",
+        },
+      
+        breakpoints: {
+            480: { 
+                slidesPerView: 1,
+                spaceBetween: 30,
+            },
+            768: { 
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 90,
+            },
         },
     });
 }
